8fef30440cec37a5d10b9acb43f2819dd3bdef36
adityaashinde <adityashinde4580@gmail.com>
2024-01-26 18:39:52 +0530

    Update palindrome.dart

--- lib/palindrome.dart
+++ lib/palindrome.dart
@@ -8,17 +8,69 @@ class Palindrome extends StatefulWidget {
 }
 
 class _PalindromeState extends State<Palindrome> {
+  int count = 0;
+  void countPalindrome() {
+    count = 0;
+
+    List<int> numbersList = [1, 225, 454, 121, 411, 234, 999];
+
+    for (int i = 0; i < numbersList.length; i++) {
+      int temp = numbersList[i].abs();
+      int reverseNum = 0;
+      while (temp != 0) {
+        reverseNum = reverseNum * 10 + temp % 10;
+        temp = temp ~/ 10;
+      }
+
+      if (reverseNum == numbersList[i].abs()) {
+        count++;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
-        title: const Text("Palindrome-Count"),
+        backgroundColor: Colors.white,
+        title: const Text(
+          "Palindrome-Count-App",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
-      body: const SizedBox(
+      body: SizedBox(
         width: double.infinity,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                countPalindrome();
+                setState(() {});
+              },
+              child: const Text(
+                "Check Palindrome",
+                style: TextStyle(
+                  // fontStyle: FontStyle.italic,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Text(
+              " $count Numbers are Palindrome ",
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              ),
+            ),
+          ],
         ),
       ),
     );
